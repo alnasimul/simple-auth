6fe5fe4181a060a076044b7b601e8f47cf6edc05
Al Nasimul Haque <alnasimul@gmail.com>
2021-01-20 00:04:41 +0600

    basic app setup done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
 import './App.css';
+import firebaseConfig from './simple-auth-config';
+
+firebase.initializeApp(firebaseConfig);
+
 
 function App() {
   return (
