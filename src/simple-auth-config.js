cbe4c6dd42a378eb73174ddcaacb9230243e35d5
Al Nasimul Haque <alnasimul@gmail.com>
2021-01-19 23:01:57 +0600

    first commit

---
+++ src/simple-auth-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIDEbklaCz-NUZah4lC2VU1CiEe8j7AJI",
+    authDomain: "simple-auth-01.firebaseapp.com",
+    projectId: "simple-auth-01",
+    storageBucket: "simple-auth-01.appspot.com",
+    messagingSenderId: "649259063000",
+    appId: "1:649259063000:web:af2b01e6a4c1f218c5a9ad"
+  };
+
+export default firebaseConfig;  
