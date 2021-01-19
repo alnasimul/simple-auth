ee2d8c3c7d026c7aefae95508f9b332082d7a34b
Al Nasimul Haque <alnasimul@gmail.com>
2021-01-19 23:08:22 +0600

    basic setup done

--- src/simple-auth-config.js
+++ src/simple-auth-config.js
@@ -4,7 +4,6 @@ const firebaseConfig = {
     projectId: "simple-auth-01",
     storageBucket: "simple-auth-01.appspot.com",
     messagingSenderId: "649259063000",
-    appId: "1:649259063000:web:af2b01e6a4c1f218c5a9ad"
+    appId: "1:649259063000:web:9fea8bb3f5133f22c5a9ad"
   };
-
 export default firebaseConfig;  
